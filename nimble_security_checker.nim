73b8f7ebc74c308a6c2324d44dde2e315d334324
Juan Carlos <JuanCarlospaco@gmail.com>
2020-05-26 17:53:41 -0300

    Add Nim instalation process to the Audit logs

--- nimble_security_checker.nim
+++ nimble_security_checker.nim
@@ -1,20 +1,19 @@
 import os, strutils, json, osproc, times
 
 const
-  nimInitCmd = "curl https://nim-lang.org/choosenim/init.sh -sSf | sh"
-  nimInstallCmd = "python3 tracexec.py " & nimInitCmd
+  nimInstallCmd = "python3 tracexec.py ./init.sh"
   checkCmd = "python3 tracexec.py nimble --noColor --verbose --accept install "
   packages = "https://raw.githubusercontent.com/nim-lang/packages/master/packages.json"
   jsn = staticExec("curl -s " & packages)
 
 let serie = char(parseInt(now().format("d")) + 96)  # Do 1 "serie" per day of month.
-
 var errors = "exitCode, name\n"
 
 
 if serie >= 'z':  # if no packages to check, check Choosenim and init.sh
-  let audit = execCmdEx(nimInstallCmd)
-  if audit.exitCode == 0: writeFile("nim_install.log", audit.output)
+  if gorgeEx("curl -LOks https://nim-lang.org/choosenim/init.sh").exitCode == 0:
+    let audit = execCmdEx(nimInstallCmd)
+    if audit.exitCode == 0: writeFile("nim_install.log", audit.output)
 else:
   for item in parseJson(jsn).items:
     let name = item.getOrDefault("name").getStr.toLowerAscii
